2e09a6bc33d9ee9be360b96ea3e30edc177bccff
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-06 12:10:58 +0530

    Techfest_and_the_Queue.cpp

--- GFG/POTD/Techfest_and_the_Queue.cpp
+++ GFG/POTD/Techfest_and_the_Queue.cpp
@@ -1,3 +1,5 @@
+//Approach-1
+
 int count_prime_factor(int i)
     {
         int count=0;
@@ -27,6 +29,54 @@ int count_prime_factor(int i)
 	    int result=0;
 	    for(int i=a;i<=b;i++)
 	    {
+	        result+=(count_prime_factor(i));
+	    }
+	    return result;
+	}
+
+
+//Approach-2_using Sieve of Eratosthenes Algorithm.
+vector<int>prime;
+    void find_prime(int n)
+    {
+        prime.resize(n+1,1);
+        prime[1]=false;
+        for (int p=2;p<=n;p++)
+        {
+            if (prime[p] == true) 
+            {
+                for (int i=p+p;i<=n;i+=p)
+                    prime[i] = false;
+            }
+        }
+    }
+    int count_prime_factor(int i)
+    {
+        int count=0;
+        for(int j=2;j<=sqrt(i);j++)
+        {
+            while(i%j==0)
+            {
+                i/=j;
+                count++;
+            }
+            if(prime[i])return count+1;
+        }
+        return count;
+    }
+	int sumOfPowers(int a, int b)
+	{
+	    int result=0;
+	   // vector<int>primes;
+        find_prime(b+1);
+	    for(int i=a;i<=b;i++)
+	    {
+	        if(i==1)continue;
+	        if(i<=3)
+	        {
+	            result++;
+	            continue;
+	        }
 	        result+=(count_prime_factor(i));
 	    }
 	    return result;
