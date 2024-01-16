b1bc0dd882fd0bc2e28003bb9e37238634750537
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-16 18:31:21 +0530

    Create Chocolate Distribution.cpp

---
+++ Code_chef/contest_question/Chocolate Distribution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	int stress;
+	cin>>stress;
+	while(stress--)
+    {
+        int n;
+        cin>>n;
+        cout<<(n+1)/2<<" "<<n<<endl;
+    }
+}
