2a78af128e293042f1d2a61f72a44bce93eb55a7
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-17 23:21:09 +0530

    Create Spiral Matrix.cpp

---
+++ GFG/Matrix/Spiral Matrix.cpp
@@ -0,0 +1,35 @@
+int findK(vector<vector<int>> &matrix, int r, int c, int h)
+    {
+        vector<int>ans;
+        int i,k=0,l=0;
+        while(k<r&&l<c)
+        {
+            for(i=l;i<c;i++)
+            {
+                ans.push_back(matrix[k][i]);
+            }
+            k++;
+            for(i=k;i<r;i++)
+            {
+                ans.push_back(matrix[i][c-1]);
+            }
+            c--;
+            if(k<r)
+            {
+                for(int i=c-1;i>=l;i--)
+                {
+                    ans.push_back(matrix[r-1][i]);
+                }
+                r--;
+            }
+            if(l<c)
+            {
+                for(int i=r-1;i>=k;i--)
+                {
+                    ans.push_back(matrix[i][l]);
+                }
+                l++;
+            }
+        }
+        return ans[h-1];
+    }
