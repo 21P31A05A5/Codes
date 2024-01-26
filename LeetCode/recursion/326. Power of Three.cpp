2497a5452d0a4bc685a6ec51faefee57aa6d8d4b
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-26 18:56:56 +0530

    Create 326. Power of Three.cpp

---
+++ LeetCode/recursion/326. Power of Three.cpp
@@ -0,0 +1,7 @@
+bool isPowerOfThree(int n) 
+    {
+        if(n == 1) return true;
+        if(n == 0) return false;
+        if(n % 3 != 0) return false;
+        return isPowerOfThree(n/3);
+    }
