446c8c6c736fa0a5670178f31c5c3ba858848e0e
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-02-01 22:03:28 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- GFG/DP/0 - 1 Knapsack Problem.cpp
+++ GFG/DP/0 - 1 Knapsack Problem.cpp
@@ -1,3 +1,4 @@
+//Memoization Approach
 int dp[1001][1001];
 class Solution
 {
@@ -24,3 +25,33 @@ class Solution
        return helper(W,wt,val,n);
     }
 };
+
+//Top-Down DP
+int knapSack(int w, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+       for(int i=0;i<=n;i++)
+       {
+           dp[i][0]=0;
+       }
+       for(int i=0;i<=w;i++)
+       {
+           dp[0][i]=0;
+       }
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=w;j++)
+           {
+               if(wt[i-1]<=j)
+               {
+                   dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+               }
+               else
+               {
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       }
+       return dp[n][w];
+    }
