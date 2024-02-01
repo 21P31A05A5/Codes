e45dd208e800efb7745b5df501796775a9ffc781
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-02-01 23:23:09 +0530

    Update Subset Sum Problem.cpp

--- GFG/DP/Subset Sum Problem.cpp
+++ GFG/DP/Subset Sum Problem.cpp
@@ -1,3 +1,4 @@
+//Memoization Approach
 bool subset(int n,vector<int>arr,int sum,vector<vector<int>>&dp)
     {
         // if(n==0)return false;
@@ -27,4 +28,35 @@ bool subset(int n,vector<int>arr,int sum,vector<vector<int>>&dp)
             dp[i][0]=true;
         }
         return subset(n,arr,sum,dp);
+    }
+
+//top-down approach
+bool isSubsetSum(vector<int>arr, int sum)
+    {
+        // code here 
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        for(int i=0;i<=sum;i++)
+        {
+            dp[0][i]=false;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    dp[i][j]=(dp[i-1][j-arr[i-1]])||dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
     }
