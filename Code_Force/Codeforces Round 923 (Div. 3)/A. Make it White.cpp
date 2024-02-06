ba4c617e1ea1ab0639385c35b4a8fca27fc5ba29
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-02-06 22:11:42 +0530

    Create A. Make it White.cpp

---
+++ Code_Force/Codeforces Round 923 (Div. 3)/A. Make it White.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        int n;
+        cin>>n;
+        string ss;
+        cin>>ss;
+        int start;
+        int end;
+        for(int i=0; i<n; i++)
+        {
+            if(ss[i] == 'B')
+            {
+                start = i;
+                break;
+            }
+        }
+        for(int i=n-1; i>=0; i--)
+        {
+            if(ss[i] == 'B')
+            {
+                end = i;
+                break;
+            }
+        }
+        cout<<end-start+1<<endl;
+    }
+    
+}
