eb2b392b15239c8a575eeb94007ffa1e9fdba6ea
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-02-08 23:57:48 +0530

    Create Check if all leaves are at same level.cpp

---
+++ GFG/POTD/Check if all leaves are at same level.cpp
@@ -0,0 +1,29 @@
+int ans;
+    void solve(Node *root , int h , int &ma)
+    {
+        if(!root) return;
+        if(ans==0) return;
+        if(!root->left && !root->right)
+        {
+            if(ma==-1)
+            {
+               ma=h; 
+            }
+            else
+            {
+                if(h!=ma) ans=0;
+            }
+            return;
+        }
+        solve(root->left,h+1,ma);
+        solve(root->right,h+1, ma);
+    }
+    bool check(Node *root)
+    {
+        //Your code here
+        int ma=-1;
+        ans=1;
+        int h=0;
+        solve(root,h,ma);
+        return ans;
+    }
