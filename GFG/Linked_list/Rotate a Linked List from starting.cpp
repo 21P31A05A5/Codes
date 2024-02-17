bc94670c120c1f1e5e49f531e5681d439b998fd7
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-02-17 18:17:27 +0530

    Create Rotate a Linked List from starting.cpp

---
+++ GFG/Linked_list/Rotate a Linked List from starting.cpp
@@ -0,0 +1,30 @@
+Node* findNthNode(Node* temp,int k)
+    {
+        int cnt=1;
+        while(temp!=NULL)
+        {
+            if(cnt==k)return temp;
+            cnt++;
+            temp=temp->next;
+        }
+        return temp;
+    }
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+        if(k==0||head==NULL)return head;
+        int len=1;
+        Node* tail=head;
+        while(tail->next!=NULL)
+        {
+            tail=tail->next;
+            len+=1;
+        }
+        if(k%len==0)return head;
+        k=k%len;
+        tail->next=head;
+        Node* newLastNode=findNthNode(head,k);
+        head=newLastNode->next;
+        newLastNode->next=NULL;
+        return head;
+    }
