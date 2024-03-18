6e9be5ceacd83d8ecb194bae25043488b5bffec2
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-03-18 21:39:30 +0530

    Create Level order traversal.cpp

---
+++ GFG/POTD/Level order traversal.cpp
@@ -0,0 +1,16 @@
+vector<int> levelOrder(Node* root)
+    {
+        //Your code here
+        queue<Node*>q;
+        q.push(root);
+        vector<int>ans;
+        while(!q.empty())
+        {
+            Node* node=q.front();
+            q.pop();
+            ans.push_back(node->data);
+            if(node->left)q.push(node->left);
+            if(node->right)q.push(node->right);
+        }
+        return ans;
+    }
