2b37ab8bb1b103800fd398dccfd92d1f062f903b
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-04-29 21:07:38 +0530

    Create Remove every kth node.cpp

---
+++ GFG/POTD/Remove every kth node.cpp
@@ -0,0 +1,25 @@
+Node* deleteK(Node *head,int K)
+    {
+        if(K==1)
+        {
+            return NULL;
+        }
+        Node* temp=head;
+        Node* pre=NULL;
+        long long count=1;
+        while(temp!=NULL)
+        {
+            if(count==K)
+            {
+                pre->next=temp->next;
+                count=0;
+            }
+            else
+            {
+                pre=temp;
+            }
+            temp=temp->next;
+            count++;
+        }
+        return head;
+    }
